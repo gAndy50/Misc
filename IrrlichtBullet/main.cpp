67928dc59a494028da2b72911db80dfca14cb15c
Andy <g_andy@live.com>
2023-06-30 16:51:33 -0700

    Softbody added

--- IrrlichtBullet/main.cpp
+++ IrrlichtBullet/main.cpp
@@ -3,6 +3,11 @@
 #include <irrlicht.h>
 
 #include <btBulletDynamicsCommon.h>
+#include <BulletSoftBody/btSoftRigidDynamicsWorld.h>
+#include <BulletSoftBody/btSoftBodyHelpers.h>
+#include <BulletSoftBody/btDeformableBodySolver.h>
+#include <BulletSoftBody/btDeformableMultiBodyDynamicsWorld.h>
+#include <BulletSoftBody/btSoftBodyRigidBodyCollisionConfiguration.h>
 
 using namespace irr;
 using namespace core;
@@ -11,6 +16,7 @@ using namespace scene;
 using namespace io;
 using namespace gui;
 
+//Event class for input handling
 class InputEventHandler : public IEventReceiver
 {
 public:
@@ -53,9 +59,17 @@ btBroadphaseInterface* broadphase;
 btSequentialImpulseConstraintSolver* solver;
 btDiscreteDynamicsWorld* world;
 
+btSoftBodyWorldInfo softBodyWorld;
+btDeformableBodySolver* deform_solver;
+btDeformableMultiBodyDynamicsWorld* deform_world;
+btDeformableMultiBodyConstraintSolver* deform_sol;
+
 btAlignedObjectArray<btCollisionShape*> collide_shapes;
 
 int lastFPS = -1;
+ISceneManager* Smgr;
+IVideoDriver* Driver;
+btSoftBody* cloth;
 
 void InitPhysics();
 void ExitPhysics();
@@ -64,9 +78,14 @@ void StepPhysics(float timeStep);
 void PhysicsGround();
 void AddBox(ISceneManager* Smgr, IVideoDriver* Driver);
 void AddSphere(ISceneManager* Smgr, IVideoDriver* Driver);
+
 void DrawPhysicsObjects();
+void DrawSoftBodies();
+void DrawDeformPhysics();
 
 void addChain(int chainCount, const btVector3& suspensionPoint, ISceneManager* Smgr, IVideoDriver* Driver);
+
+//void addSoftBody(const btScalar s, const int numX, const int numY, const int fixed = 3);
 
 void ShootBox(ISceneManager* Smgr, IVideoDriver* Driver, vector3df& position, vector3df& lookat);
 void ShootSphere(ISceneManager* Smgr, IVideoDriver* Driver, vector3df& position, vector3df& lookat);
@@ -109,6 +128,64 @@ btRigidBody* createRigidBody(btDiscreteDynamicsWorld* world, float mass, const b
 	return body;
 }
 
+void addSoftBody(const btScalar& s, const int numX, const int numY, const int fixed = 3)
+{
+	cloth = btSoftBodyHelpers::CreatePatch(softBodyWorld,
+		btVector3(-s / 2, s + 1, 0),
+		btVector3(+s / 2, s + 1, 0),
+		btVector3(-s / 2, s + 1, +s),
+		btVector3(+s / 2, s + 1, +s),
+		numX, numY, fixed, true);
+
+	cloth->getCollisionShape()->setMargin(0.0001f);
+	btSoftBody::Material* pm = cloth->appendMaterial();
+	pm->m_kLST = 0.004;
+	pm->m_kAST = 0.004;
+	pm->m_kVST = 0.004;
+	cloth->generateBendingConstraints(2, pm);
+	cloth->setTotalMass(39);
+	cloth->m_cfg.kKHR = 1;
+	cloth->m_cfg.kCHR = 1;
+	cloth->m_cfg.kDF = 3;
+	cloth->m_cfg.collisions = btSoftBody::fCollision::SDF_RD;
+	cloth->m_cfg.collisions |= btSoftBody::fCollision::SDF_RDF;
+	cloth->m_cfg.collisions |= btSoftBody::fCollision::VF_DD;
+
+	cloth->m_cfg.piterations = 2;
+	cloth->m_cfg.kDP = 0.0008f;
+	cloth->m_cfg.kLF = 0.008;
+	cloth->m_cfg.kDG = 0.0006;
+	cloth->m_cfg.kKHR = 1;
+	cloth->m_cfg.kCHR = 1;
+	cloth->m_cfg.kDF = 3;
+	cloth->m_cfg.collisions = btSoftBody::fCollision::SDF_RD;
+	cloth->m_cfg.collisions |= btSoftBody::fCollision::SDF_RDF;
+	cloth->m_cfg.collisions |= btSoftBody::fCollision::VF_DD;
+	cloth->m_cfg.aeromodel = btSoftBody::eAeroModel::V_TwoSidedLiftDrag;
+	cloth->m_cfg.collisions = btSoftBody::fCollision::CL_SS + btSoftBody::fCollision::CL_RS;
+
+	cloth->generateClusters(30e7);
+
+	cloth->setWindVelocity(btVector3(20, -90.0, -9.0));
+
+	((btSoftRigidDynamicsWorld*)world)->addSoftBody(cloth);
+
+	IAnimatedMesh* hillPlaneMesh = Smgr->addHillPlaneMesh("Curtain",
+		core::dimension2d<f32>(4, 4),
+		core::dimension2d<u32>(numX - 1, numY - 1), 0, 0,
+		core::dimension2d<f32>(0, 0),
+		core::dimension2d<f32>(10, 10));
+
+	ISceneNode* Node = Smgr->addAnimatedMeshSceneNode(hillPlaneMesh);
+	Node->setMaterialTexture(0, Driver->getTexture("D:/Github/Irrlicht-1.8.5/media/wall.bmp"));
+	Node->setMaterialFlag(video::EMF_LIGHTING, false);
+	Node->setMaterialFlag(video::EMF_BACK_FACE_CULLING, false);
+	Node->setPosition(core::vector3df(0, -40, 0));
+
+	cloth->setUserPointer(hillPlaneMesh);
+	IMeshBuffer* buffer = hillPlaneMesh->getMeshBuffer(0);
+}
+
 int main(int argc, char* argv[])
 {
 	InputEventHandler receiver;
@@ -117,8 +194,8 @@ int main(int argc, char* argv[])
 	if (!Device)
 		return 1;
 
-	video::IVideoDriver* Driver = Device->getVideoDriver();
-	scene::ISceneManager* Smgr = Device->getSceneManager();
+	 Driver = Device->getVideoDriver();
+	 Smgr = Device->getSceneManager();
 	gui::IGUIEnvironment* Env = Device->getGUIEnvironment();
 
 	InitPhysics();
@@ -132,12 +209,13 @@ int main(int argc, char* argv[])
 	//The ground
 	IAnimatedMesh* hillPlaneMesh = Smgr->addHillPlaneMesh("myHill", dimension2d<f32>(30, 30), dimension2d<u32>(10, 10), 0, 0, dimension2d<f32>(0, 0), dimension2d<f32>(10, 10));
 	ISceneNode* planeNode = Smgr->addAnimatedMeshSceneNode(hillPlaneMesh);
-	planeNode->setMaterialTexture(0, Driver->getTexture("/media/stones.jpg"));
+	planeNode->setMaterialTexture(0, Driver->getTexture("D:/Github/irrlicht-1.8.5/media/stones.jpg"));
 	planeNode->setMaterialFlag(video::EMF_LIGHTING, false);
 	planeNode->setPosition(core::vector3df(0, -55, 0));
 
-	Camera = Smgr->addCameraSceneNodeFPS(NULL, 20.0f, 0.02f);
+	Camera = Smgr->addCameraSceneNodeFPS(0, 20.0f, 0.02f);
 	Camera->setPosition(vector3df(0, 10, -200));
+	Device->getCursorControl()->setVisible(true);
 
 	u32 then = Device->getTimer()->getTime();
 
@@ -145,6 +223,7 @@ int main(int argc, char* argv[])
 	{
 		const u32 now = Device->getTimer()->getTime();
 		DrawPhysicsObjects();
+		DrawSoftBodies();
 
 		if (receiver.IsKeyDown(KEY_ESCAPE))
 			break;
@@ -173,6 +252,24 @@ int main(int argc, char* argv[])
 		{
 			addChain(10, btVector3(10 + (-30 + rand() % 100), 10, 20 + (10 + rand() % 30)), Smgr, Driver);
 			receiver.Reset(KEY_KEY_H);
+		}
+
+		if (receiver.IsKeyDown(KEY_KEY_A))
+		{
+			receiver.Reset(KEY_KEY_C);
+			if (!cloth)
+				addSoftBody(4 * 20, 20, 20);
+		}
+
+		if (receiver.IsKeyDown(KEY_KEY_W))
+		{
+			if (cloth)
+				if (cloth->getWindVelocity().x() == 0)
+					cloth->setWindVelocity(btVector3(25, -90.0, -29.0));
+				else
+					cloth->setWindVelocity(btVector3(0., 0.0, 0.0));
+
+			receiver.Reset(KEY_KEY_W);
 		}
 
 		if (receiver.IsKeyDown(KEY_KEY_V))
@@ -214,13 +311,25 @@ int main(int argc, char* argv[])
 
 void InitPhysics()
 {
-	collisionConfig = new btDefaultCollisionConfiguration();
+	collisionConfig = new btSoftBodyRigidBodyCollisionConfiguration();
 	dispatcher = new btCollisionDispatcher(collisionConfig);
 	broadphase = new btDbvtBroadphase();
 	solver = new btSequentialImpulseConstraintSolver();
-	world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfig);
-
+	world = new btSoftRigidDynamicsWorld(dispatcher, broadphase, solver, collisionConfig);
+	
 	world->setGravity(btVector3(0, -10, 0));
+	world->getDispatchInfo().m_enableSPU = true;
+
+	deform_solver = new btDeformableBodySolver();
+	deform_sol = new btDeformableMultiBodyConstraintSolver();
+	deform_sol->setDeformableSolver(deform_solver);
+
+	deform_world = new btDeformableMultiBodyDynamicsWorld(dispatcher, broadphase, deform_sol, collisionConfig, deform_solver);
+
+	softBodyWorld.m_broadphase = broadphase;
+	softBodyWorld.m_dispatcher = dispatcher;
+	softBodyWorld.m_gravity = world->getGravity();
+	softBodyWorld.m_sparsesdf.Initialize();
 }
 
 void PhysicsGround()
@@ -260,7 +369,7 @@ void AddBox(ISceneManager* Smgr, IVideoDriver* Driver)
 
 	btScalar mass(3.f);
 
-	transform.setOrigin(btVector3(10, 40, 20));
+	transform.setOrigin(btVector3(rand() % 10, rand() % 40, rand() % 20));
 	btQuaternion quat(btVector3(0.4, .02, .1), 67);
 	transform.setRotation(quat);
 
@@ -270,7 +379,7 @@ void AddBox(ISceneManager* Smgr, IVideoDriver* Driver)
 
 	IMeshSceneNode* cubeNode = Smgr->addCubeSceneNode(10.0f, NULL, -1, vector3df(0, 3, 10));
 	cubeNode->setMaterialType(EMT_SOLID);
-	cubeNode->setMaterialTexture(0, Driver->getTexture("/media/wall.jpg"));
+	cubeNode->setMaterialTexture(0, Driver->getTexture("D:/Github/irrlicht-1.8.5/media/wall.jpg"));
 	cubeNode->setMaterialFlag(video::EMF_LIGHTING, false);
 	cubeNode->setVisible(false);
 
@@ -287,7 +396,7 @@ void AddSphere(ISceneManager* Smgr, IVideoDriver* Driver)
 
 	btScalar mass(10.f);
 
-	transform.setOrigin(btVector3(5, 40, 20));
+	transform.setOrigin(btVector3(rand() % 5, rand()% 40,rand() % 20));
 	btQuaternion quat(btVector3(0.4, .02, .1), 67);
 	transform.setRotation(quat);
 
@@ -296,8 +405,8 @@ void AddSphere(ISceneManager* Smgr, IVideoDriver* Driver)
 	body->setUserIndex(10);
 
 	IMeshSceneNode* sphereNode = Smgr->addSphereSceneNode(10.0f, 32);
-	sphereNode->setMaterialType(EMT_SOLID);
-	sphereNode->setMaterialTexture(0, Driver->getTexture("/media/water.jpg"));
+	sphereNode->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+	sphereNode->setMaterialTexture(0, Driver->getTexture("D:/Github/irrlicht-1.8.5/media/water.jpg"));
 	sphereNode->setMaterialFlag(video::EMF_LIGHTING, false);
 	sphereNode->setVisible(false);
 
@@ -330,7 +439,7 @@ void addChain(int chainCount, const btVector3& suspensionPoint, ISceneManager* S
 
 		IMeshSceneNode* Node = Smgr->addCubeSceneNode(8.0f, NULL, -1, vector3df(0, 0, 0), vector3df(0, 0, 0), vector3df(1, 1, 0.25f));
 		Node->setMaterialType(EMT_SOLID);
-		Node->setMaterialTexture(0, Driver->getTexture("/media/wall.jpg"));
+		Node->setMaterialTexture(0, Driver->getTexture("D:/Github/irrlicht-1.8.5/media/wall.jpg"));
 		Node->setMaterialFlag(video::EMF_LIGHTING, false);
 		boxes[i]->setUserPointer(Node);
 	}
@@ -378,7 +487,7 @@ void ShootBox(ISceneManager* Smgr, IVideoDriver* Driver, vector3df& position, ve
 
 	ISceneNode* Node = Smgr->addCubeSceneNode(10.0f, NULL, -1, vector3df(0, 3, 10));
 	Node->setMaterialType(EMT_SOLID);
-	Node->setMaterialTexture(0, Driver->getTexture("/media/wall.jpg"));
+	Node->setMaterialTexture(0, Driver->getTexture("D:/Github/irrlicht-1.8.5/media/wall.jpg"));
 	Node->setMaterialFlag(video::EMF_LIGHTING, false);
 	Node->setVisible(false);
 
@@ -418,7 +527,7 @@ void ShootSphere(ISceneManager* Smgr, IVideoDriver* Driver, vector3df& position,
 
 	IMeshSceneNode* sphereNode = Smgr->addSphereSceneNode(10.0f, 32);
 	sphereNode->setMaterialType(EMT_SOLID);
-	sphereNode->setMaterialTexture(0, Driver->getTexture("/media/water.jpg"));
+	sphereNode->setMaterialTexture(0, Driver->getTexture("D:/Github/irrlicht-1.8.5/media/water.jpg"));
 	sphereNode->setMaterialFlag(video::EMF_LIGHTING, false);
 	sphereNode->setVisible(false);
 
@@ -428,6 +537,44 @@ void ShootSphere(ISceneManager* Smgr, IVideoDriver* Driver, vector3df& position,
 
 	world->addRigidBody(body);
 	body->applyImpulse(350 * force, btVector3(0., 0., 0.0));
+}
+
+void DrawSoftBodies()
+{
+	btSoftRigidDynamicsWorld* softWorld = ((btSoftRigidDynamicsWorld*)world);
+	btSoftBodyArray sarray = softWorld->getSoftBodyArray();
+	if (sarray.size() > 0)
+	{
+		btSoftBody* psb = (btSoftBody*)sarray[0];
+		if (psb)
+		{
+			IAnimatedMesh* plane = (IAnimatedMesh*)psb->getUserPointer();
+			IMeshBuffer* buffer = plane->getMeshBuffer(0);
+
+			for (int i = 0; i < buffer->getVertexCount(); i++)
+			{
+				const btSoftBody::Node& n = psb->m_nodes[i];
+				btVector3 pos = n.m_x;
+				btVector3 normal = n.m_n;
+
+				buffer->getPosition(i).X = pos.x();
+				buffer->getPosition(i).Y = pos.y();
+				buffer->getPosition(i).Z = pos.z();
+
+				buffer->getNormal(i).X = normal.x();
+				buffer->getNormal(i).Y = normal.y();
+				buffer->getNormal(i).Z = normal.z();
+			}
+		}
+	}
+}
+
+void DrawDeformPhysics()
+{
+	for (int i = 0; i < deform_world->getSoftBodyArray().size(); i++)
+	{
+		btSoftBody* psb = (btSoftBody*)deform_world->getSoftBodyArray()[i];
+	}
 }
 
 void DrawPhysicsObjects()
@@ -498,10 +645,17 @@ void ExitPhysics()
 	}
 
 	delete world;
+	world = 0;
 	delete solver;
+	solver = 0;
 	delete broadphase;
+	broadphase = 0;
 	delete dispatcher;
+	dispatcher = 0;
 	delete collisionConfig;
+	collisionConfig = 0;
+
+	delete deform_solver;
 
 	collide_shapes.clear();
 }
